32470ea552028f76fe57d803315746c5ec2a2e0a
unknown <lindsay.kelly15@gmail.com>
2022-03-31 18:27:32 -0400

    done

---
+++ data/migrations/20220331222149_create-tables.js
@@ -0,0 +1,12 @@
+
+exports.up = function (knex) {
+    return knex.schema.createTable('testing', (table) => {
+      table.increments('testing_id');
+      table.string('full_name').notNullable().unique();
+      table.string('email').notNullable().unique();
+    });
+  };
+  
+  exports.down = function (knex) {
+    return knex.schema.dropTableIfExists('testing');
+  };
